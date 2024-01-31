f14977095ae74a84a3ead63d26eecf7fe82d5a91
tihiswasavailable <stefan.jovic@edu.fh-joanneum.at>
2024-01-31 01:44:46 +0100

    db.createConnection

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mysql2 = require("mysql2");
+
 const app = express();
+const db = mysql2.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'moviefy-login'
+});
+
+db.connect( (error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MYSQL Connected...")
+    }
+})
 
 app.get("/", (req, res) => {
     res.send("<h1>Homepage</h1>")
 });
 
-app.listen(5501, () => {
-    console.log("Server started on Port 5501")
+app.listen(3306, () => {
+    console.log("Server started on Port 3306")
 });
