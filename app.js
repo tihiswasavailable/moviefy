f53caabcb40c182bb96be95873d0afc09ba6b264
tihiswasavailable <stefan.jovic@edu.fh-joanneum.at>
2024-01-31 03:38:36 +0100

    fiished db connection via mysql

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const mysql2 = require("mysql2");
+const mysql = require("mysql2");
 
 const app = express();
-const db = mysql2.createConnection({
+
+const db = mysql.createConnection({
     host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
+    user: 'moviefy_user',
+    password: '0305',
     database: 'moviefy-login'
 });
 
-db.connect( (error) => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log("MYSQL Connected...")
-    }
-})
+db.connect(err => {
+    if (err) {
+        console.log("Error connecting to database") + err.stack;
+        return;
+    } 
+    console.log("Connected to database");
+});
 
 app.get("/", (req, res) => {
     res.send("<h1>Homepage</h1>")
 });
 
-app.listen(3306, () => {
-    console.log("Server started on Port 3306")
+app.listen(5001, () => {
+    console.log("Server started on Port 5001")
 });
